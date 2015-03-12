7bdb74477e945481eca1ce556daeb43989fbc6f7
nikhilbedi <nbedi@usc.edu>
2015-03-11 23:05:31 -0700

    Achieved flashing triangles with constant buffers. Need to still understand timing correctly to have it to do it exactly every one second.

--- Game/Data/Shaders/App3_2.hlsl
+++ Game/Data/Shaders/App3_2.hlsl
@@ -1,2 +1,52 @@
-//lab 3
-//implement!
+/*
+struct VS_INPUT {
+	float4 mPos : POSITION;
+	float3 mColor : COLOR;
+};
+
+struct PS_INPUT {
+	float4 mPos : SV_POSITION;
+	float3 mColor : TEXCOORD0;
+};
+
+//--------------------------------------------------------------------------------------
+// Vertex Shader
+//--------------------------------------------------------------------------------------
+PS_INPUT VS(VS_INPUT input)
+{
+	PS_INPUT output;
+	output.mPos = input.mPos;
+	output.mColor = input.mColor;
+	return output;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Pixel Shader
+//--------------------------------------------------------------------------------------
+float4 PS(PS_INPUT input) : SV_Target
+{
+	return input.mColor;
+}
+*/
+cbuffer MyConstants : register(b0)
+{
+	float4 gColor;
+};
+
+//--------------------------------------------------------------------------------------
+// Vertex Shader
+//--------------------------------------------------------------------------------------
+float4 VS(float4 Pos : POSITION) : SV_POSITION
+{
+	return Pos;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Pixel Shader
+//--------------------------------------------------------------------------------------
+float4 PS(float4 Pos : SV_POSITION) : SV_Target
+{
+	return gColor;
+}
--- Game/Engine/main/src/App3_2.cpp
+++ Game/Engine/main/src/App3_2.cpp
@@ -3,21 +3,112 @@
 
 namespace ITP485
 {
+	struct MyConstants
+	{
+		Quaternion color;
+	};
+	MyConstants constants;
+	GraphicsBufferPtr mPixelShaderConstantBuffer;
+	int colorSwitch = 0;
+	double counter = 0;
+
 	App3_2::App3_2()
 	{
 		//copy from App3_1 and modify
+		//let's make some shaders! Here's the code from lecture to load up the vertex shader in App3_1.hlsl
+		vector< char > compiledVertexShader;
+		GraphicsDriver::Get()->CompileShaderFromFile(L"Shaders\\App3_2.hlsl", "VS", "vs_4_0", compiledVertexShader);
+		mVertexShader = GraphicsDriver::Get()->CreateVertexShader(compiledVertexShader);
+
+		//now load up the pixel shader named PS
+		vector< char > compiledPixelShader;
+		GraphicsDriver::Get()->CompileShaderFromFile(L"Shaders\\App3_2.hlsl", "PS", "ps_4_0", compiledPixelShader);
+		mPixelShader = GraphicsDriver::Get()->CreatePixelShader(compiledPixelShader);
+
+		//now create an input layout to describe vertices that contain 3 floats for position data and nothing else
+		InputLayoutElement *inputLayoutElement = new InputLayoutElement("POSITION", 0, EGF_R32G32B32_Float, 0);
+		InputLayoutPtr inputLayoutPtr =
+			GraphicsDriver::Get()->CreateInputLayout(inputLayoutElement, 1, compiledVertexShader);	// first parameter is an array of inputLayout elements!
+
+		//now set it in the graphics driver
+		GraphicsDriver::Get()->SetInputLayout(inputLayoutPtr);
+
+		// Create vertex buffer
+		float vertices[] =
+		{
+			0.0f, 0.5f, 0.5f,
+			0.5f, -0.5f, 0.5f,
+			-0.5f, -0.5f, 0.5f
+		};
+
+		mVertexBuffer = GraphicsDriver::Get()->CreateGraphicsBuffer(
+			vertices,
+			sizeof(vertices),
+			EBF_VertexBuffer,
+			0,
+			EGBU_Immutable);
+
+		// Set up constant buffer
+		constants.color.Set(0.0f, 0.0f, 1.0f, 1.0f);
+		mPixelShaderConstantBuffer = GraphicsDriver::Get()->CreateGraphicsBuffer(
+			&constants,
+			sizeof(constants),
+			EBF_ConstantBuffer,
+			ECPUAF_CanWrite,
+			EGBU_Dynamic);
+		//GraphicsDriver::Get()->SetPSConstantBuffer(mConstantBuffer, 0);
 	}
 
 
 	void App3_2::Update()
 	{
 		//change color once per second, green, purple, red, repeat
+		counter += Timing::Get().GetTime();
+		if (counter > 10000.0f)
+		{
+			counter = 0;
+			colorSwitch++;
+			//GraphicsDriver::Get()->MapBuffer(mPixelShaderConstantBuffer);
+			switch (colorSwitch % 3)
+			{
+			case 0:
+				constants.color.Set(0.0f, 0.0f, 1.0f, 1.0f);	// Green
+				break;
+			case 1:
+				constants.color.Set(1.0f, 1.0f, 0.0f, 1.0f);	// Purple
+				break;
+			case 2:
+				constants.color.Set(1.0f, 0.0f, 0.0f, 1.0f);	// Red
+				break;
+			}
+			mPixelShaderConstantBuffer = GraphicsDriver::Get()->CreateGraphicsBuffer(
+				&constants,
+				sizeof(constants),
+				EBF_ConstantBuffer,
+				ECPUAF_CanWrite,
+				EGBU_Dynamic);
+			//GraphicsDriver::Get()->UnmapBuffer(mPixelShaderConstantBuffer);
+		}
 	}
 
 	void App3_2::Render()
 	{
 		//lab 3
 		//copy from app3_1 and modify
+		//clear back buffer,
+		GraphicsDriver::Get()->ClearBackBuffer();
+		//Set Constants Buffer
+		GraphicsDriver::Get()->SetPSConstantBuffer(mPixelShaderConstantBuffer, 0);
+		//set vertex buffer,
+		GraphicsDriver::Get()->SetVertexBuffer(mVertexBuffer, 12); // size of each vertex
+		//set vertex shader
+		GraphicsDriver::Get()->SetVertexShader(mVertexShader);
+		//set pixel shader,
+		GraphicsDriver::Get()->SetPixelShader(mPixelShader);
+		//draw
+		GraphicsDriver::Get()->Draw(3, 0); // how many vertices
+		//present!
+		GraphicsDriver::Get()->Present();
 	}
 
 }
